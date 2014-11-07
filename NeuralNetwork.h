659b3b7603e3aaf70bddeaa997ca4613f795e3f7
Pranav <pranav8iisc@gmail.com>
2014-11-07 19:56:51 +0530

    added new member functions

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -3,6 +3,11 @@ Classes defining generic structures for neural networks. These classes may later
 */
 #include <list>
 
+#define BACKPROP 1
+
+
+
+
 using namespace std;
 
 // defines neuron class
@@ -27,7 +32,7 @@ class Neuron
 };
 
 // defines the generic neural network
-template<class WeightDataType, class SignalDataType>
+template<class WeightDataType, class SignalDataType, class DatasetReaderType>
 class NeuralNetwork
 {
 	public:
@@ -45,8 +50,55 @@ class NeuralNetwork
 		WeightDataType*** hiddenToHiddenConnectionWeights;
 		WeightDataType** hiddenToOutputConnectionWeights;
 
+
+		DatasetReaderType* trainDataset;
+		DatasetReaderType* testDataset;
+
 		void readTrainingData();
 		void extractFeatures(); 
 		void trainNetwork();
 		void testNetwork();
+		
+		void getNumberOfHiddenLayers();
+		void getNumberOfOutputLayers();
+
+	protected:
+		unsigned int getNumberOfInputNeurons();
 };
+
+
+
+template<class WeightDataType, class SignalDataType, class DatasetReaderType>
+void NeuralNetwork<WeightDataType, SignalDataType, DatasetReaderType>::readTrainingData()
+{
+	trainDataset.getDataset();
+}
+
+template<class WeightDataType, class SignalDataType, class DatasetReaderType>
+void NeuralNetwork<WeightDataType, SignalDataType, DatasetReaderType>::extractFeatures()
+{
+	// ADDRESSES THE CASE WHEN FULL IMAGE IS GIVEN AS FEATURE
+	nInputNeurons = trainDataset.sizeOfDimension[1]*trainDataset.sizeOfDimension[2];
+}
+
+template<class WeightDataType, class SignalDataType, class DatasetReaderType>
+void NeuralNetwork<WeightDataType, SignalDataType, DatasetReaderType>::trainNetwork()
+{
+	
+	unsigned int methodId;
+
+	cout << "Please enter training method code:";
+	cin >> methodId;
+	
+	switch(methodId):
+	{
+		case BACKPROP:
+	}
+	
+			
+	return;
+}
+
+
+
+
